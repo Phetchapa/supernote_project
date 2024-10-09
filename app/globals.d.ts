95f86c705d5ddec83b57047b8b3a82e62181a15e
Phetchapa <phetchapa.n@kkumail.com>
2024-10-10 02:22:18 +0700

    add globals miw's code
    calendar

---
+++ app/globals.d.ts
@@ -0,0 +1,10 @@
+// global.d.ts
+
+declare global {
+    interface Window {
+      gapi: any; // Declares 'gapi' as 'any' (you can later specify the actual types if needed)
+      google: any; // Declares 'google' for Google Identity Services
+    }
+  }
+  
+  export {};
